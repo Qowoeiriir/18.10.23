c531153eee07bfa39eec2db612c3b662c5337f71
Qowoeiriir <144234414+Qowoeiriir@users.noreply.github.com>
2023-10-18 10:03:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# # № 1
+# def de_none(pink):
+#     new_pink = []
+#
+#     for i in pink:
+#         if i != None:
+#             new_pink.append(i)
+#
+#     return new_pink
+#
+# lst = [None, None, 1, [], (), {}, None]
+#
+# print(de_none(lst))
+
+# # №3
+# def get_elem(d_,k_):
+#     for i in d_:
+#         if i == k_:
+#             return d_.get(i)
+#
+# d = {'a': 1, 'b': None, 'c': [1, 2]}
+# k = 'd'
+#
+# print(get_elem(d,k))
+#
+# №2
